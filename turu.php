e96071c20ed72fb3b837edc9619001484738a081
giiesaputra1 <54400059+giiesaputra1@users.noreply.github.com>
2020-07-02 15:42:23 +0700

    Update turu.php

--- turu.php
+++ turu.php
@@ -43,31 +43,6 @@ if ($json['code'] == 0) {
 	echo "$nomor --> $a";
 	echo "\n";
 }
-}
-
-function getno($id) {
-$url = "https://m.borongbareng.com/api-gateway/service-marketing/app/bargain/self/detail/$id";
-//$data = '[{"operationName":"OTPRequest","variables":{"email":"'.$email.'","otpType":"126","mode":"email","otpDigit":4},"query":"query OTPRequest($otpType: String!, $mode: String, $msisdn: String, $email: String, $otpDigit: Int) {\n  OTPRequest(otpType: $otpType, mode: $mode, msisdn: $msisdn, email: $email, otpDigit: $otpDigit) {\n    success\n    message\n    errorMessage\n    __typename\n  }\n}\n"}]';
-$headers = array();
-$headers [] = "Host: m.borongbareng.com";
-$headers [] = "Connection: close";
-$headers [] = "Accept: application/json, text/plain, */*";
-$headers [] = "language: Indonesian";
-$headers [] = "Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBcHAiLCJleHAiOjE1OTM3NjI5NTksImlhdCI6MTU5MzY3NjU1OSwidXNlciI6IntcImlkXCI6MjA4MzYsXCJuaWNrbmFtZVwiOlwiODUqKiozMjAyNFwiLFwidXNlcm5hbWVcIjpcIig2Mik4NTY3MzMyMDI0XCIsXCJhdmF0YXJcIjpcImh0dHBzOi8vYm9yb25nYmFyZW5nLWg1Lm9zcy1hcC1zb3V0aGVhc3QtNS5hbGl5dW5jcy5jb20vZGV2LzIwMjAwNDEzL2MwYmY5Njk0M2NhMTRhZTQ5NmM5MDgwNGE3ZjNmM2FkLmpwZ1wiLFwibGFuZ3VhZ2VcIjpudWxsfSJ9.CG-tLI0m-xuVSw3MGTcy_y0Ukkd1ANaLJIFs2eCFYVA";
-$headers [] = "User-Agent: Mozilla/5.0 (Linux; Android 10; MI 8 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36";
-$headers [] = "Sec-Fetch-Site: same-origin";
-$headers [] = "Sec-Fetch-Mode: cors";
-$headers [] = "Sec-Fetch-Dest: empty";
-$headers [] = "Referer: https://m.borongbareng.com/";
-$headers [] = "Accept-Encoding: gzip, deflate";
-$headers [] = "Accept-Language: en-US,en;q=0.9";
-$headers [] = "Cookie: _ga=GA1.2.513239636.1592924479; _gid=GA1.2.728015094.1593092400; _gat_gtag_UA_164611555_2=1";
-
-$getotp = request($url, $headers);
-$json = json_decode($getotp, true);
-$a = $json['data']['code'];
-return $a;
-}
 
 echo "=========================\n";
 echo "Borong Barang Slash\n";
